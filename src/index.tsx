3118d5902b8447e1bac1a05a8521598613ed39af
masashi.g <masashi.g@gmail.com>
2020-03-16 23:13:26 +0900

    implement time travel (history back)

--- src/index.tsx
+++ src/index.tsx
@@ -69,7 +69,8 @@ interface GamePropsInterface {
 
 interface GameStateInterface {
   history: Array<SquaresInterface>;
-  xIsNext: boolean
+  stepNumber: number;
+  xIsNext: boolean;
 }
 
 class Game extends React.Component<GamePropsInterface, GameStateInterface> {
@@ -79,12 +80,13 @@ class Game extends React.Component<GamePropsInterface, GameStateInterface> {
       history: [{
         squares: Array(9).fill(""),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i: number) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -95,13 +97,21 @@ class Game extends React.Component<GamePropsInterface, GameStateInterface> {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     let status;
@@ -110,6 +120,17 @@ class Game extends React.Component<GamePropsInterface, GameStateInterface> {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     return (
       <div className="game">
@@ -121,7 +142,7 @@ class Game extends React.Component<GamePropsInterface, GameStateInterface> {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
