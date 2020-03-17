93b68909e22e991a13a61098c623cf4bd65045f2
mganeko <mganeko@users.noreply.github.com>
2020-03-17 14:41:08 +0000

    Automated deployment: Tue Mar 17 14:41:08 UTC 2020 64ccf3510cdab6bf8d223d9c2de738956c64b3ea

---
+++ precache-manifest.43c427b9893f795dd9904bba0a71ba38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713cbf697a8a05f295b2bd8cdf5f479b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fc24552d21f883cfd54",
+    "url": "/static/css/main.1c2dbac9.chunk.css"
+  },
+  {
+    "revision": "21ad7f76796c39288b32",
+    "url": "/static/js/2.e2375c88.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e2375c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc24552d21f883cfd54",
+    "url": "/static/js/main.e9fe2ce6.chunk.js"
+  },
+  {
+    "revision": "f912653f55a65fed8d7a",
+    "url": "/static/js/runtime-main.8599bf03.js"
+  }
+]);
